92a2204539a71fdb8a7e2dfbf6b6b00bff64745b
Wendel Henrique <wendels@ciandt.com>
2022-10-06 10:10:10 -0300

    Aula de ARRAYS

---
+++ src/arrays2/Matrizes.java
@@ -0,0 +1,37 @@
+package arrays2;
+
+import java.util.Random;
+
+public class Matrizes {
+	public static void main(String[] args) {
+		
+		int[][] numerosAleatorios = new int [4][4];
+		
+		Random numeroRandom = new Random();
+		
+		
+		
+	}
+		
+//		double [][]notasAluno = new double [30][4];
+//		
+//		notasAluno[0][0] = 10;
+//		notasAluno[0][1] = 7;
+//		notasAluno[0][2] = 9;
+//		notasAluno[1][3] = 9.5;
+//		
+//		notasAluno[1][0] = 9;
+//		notasAluno[1][1] = 8;
+//		notasAluno[1][2] = 7;
+//		notasAluno[1][3] = 9;
+//		
+//		notasAluno[2][0] = 9;
+//		notasAluno[2][1] = 8;
+//		notasAluno[2][2] = 7;
+//		notasAluno[2][3] = 9;
+//		
+//		System.out.println("Nota aluno :"
+//				+ notasAluno[2]);
+//	}
+
+}
